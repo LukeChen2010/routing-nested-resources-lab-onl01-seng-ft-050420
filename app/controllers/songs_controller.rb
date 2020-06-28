36d8788421d6bfab228431f8ad41a582b697b360
Luke Chen <luke.chen.2010@gmail.com>
2020-06-28 02:16:18 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,8 +18,8 @@ class SongsController < ApplicationController
   end
 
   def show
-    
-    @song = Song.find(params[:id])  end
+    @song = Song.find(params[:id])  
+  end
 
   def new
     @song = Song.new
