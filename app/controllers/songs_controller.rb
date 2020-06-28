28db9278fe81b22e4dc51f841762cb410d5c5d82
Luke Chen <luke.chen.2010@gmail.com>
2020-06-28 02:15:57 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,7 @@ class SongsController < ApplicationController
       if @artist == nil
         redirect_to artists_path
       else
-        @songs = @artist.songs if
+        @songs = @artist.songs
       end
       
     else
