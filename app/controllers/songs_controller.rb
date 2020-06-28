d7e3674ef8aca7f2094045e847f928859e3a76a3
Luke Chen <luke.chen.2010@gmail.com>
2020-06-28 02:17:11 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,7 +18,18 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])  
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      
+      if @artist == nil
+        redirect_to artists_path
+      else
+        @songs = @artist.songs
+      end
+      
+    else
+      @songs = Song.all
+    end 
   end
 
   def new
